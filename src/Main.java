1d97fd2b24e76dfb45a67c60be79b7e9c9fd374e
Svetlana <burtovaya85@gmail.com>
2021-07-29 22:57:37 +0600

    Second commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public String pickColor(String str) {
+        if (str.startsWith(X = 2))
+
+        else
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        String str1 = "blacksea";
+        System.out.println("The given strings is: " + str1);
+        System.out.println("The string begins with the color: " + m.pickColor(str1));
+    }
+}
